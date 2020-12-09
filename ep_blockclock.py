965a294481c016ead38e6561d6b3bff03ece3748
Ron Stoner <stonroner@gmail.com>
2020-12-09 05:14:29 +0000

    Timestamp logging and code cleanup

--- ep_blockclock.py
+++ ep_blockclock.py
@@ -4,53 +4,75 @@ import epd2in13_V2
 import subprocess
 import json
 import time
-
+import datetime
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 
-def printToDisplay(string):
+# TODO:
+# Add in try conditions for api call failure
+# More graceful failure on failed hardware init
+# More screens / animations / sats-per-dollar
+# Screen update time to variable
+# Functionize timestamps for console/log output
+
+api = 'https://api.coinbase.com/v2/prices/spot?currency=USD'
+
+def printToDisplay(string, init_display):
+    # Create draw object and pass in the image layer (HBlackImage)
     HBlackImage = Image.new('1', (epd2in13_V2.EPD_HEIGHT, epd2in13_V2.EPD_WIDTH), 0)
-    draw = ImageDraw.Draw(HBlackImage) # Create draw object and pass in the image layer we want to work with (HBlackImage)
-    font = ImageFont.truetype('./fonts/steelfish_rounded_bd.ttf', 65) # Create our font, passing in the font file and font size
+    draw = ImageDraw.Draw(HBlackImage)
+    font = ImageFont.truetype('./fonts/steelfish_rounded_bd.ttf', 65)
     
     # Caluculate screen center based off draw.textsize and screen width & height
     w,h = font.getsize(string)
     #print("Font Width: ", w)
     #print("Font Height:", h)
-
     #print("Max Width: ", epd2in13_V2.EPD_WIDTH)
     #print("Max Height: ", epd2in13_V2.EPD_HEIGHT)  
 
     W = (epd2in13_V2.EPD_WIDTH - h)  
     H = (86 - h)
-
     #print("Output Width: ", W)
     #print("Output Height: ", H)
 
-    # Draw the text and display the buffer
-    draw.text((W, H), string, font = font, align="center", fill=255)
-    epd.display(epd.getbuffer(HBlackImage))
+    # Draw the text (white) and display the buffer (black)
+    draw.text((W, H), string, font = font, align='center', fill=255)
+    
+    # Determine if we are doing a full or partial refresh
+    global initial_display
+    if init_display == True:
+        #print(init_display, " - Full Display")
+        epd.display(epd.getbuffer(HBlackImage))
+        initial_display=False
+    else:
+        #print(init_display, " - Partial Update")
+        epd.display(epd.getbuffer(HBlackImage))
+    
+    initial_display=False
+    return initial_display
 
 def getPrice(): 
-    response = subprocess.check_output(['curl', '-s', "https://api.coinbase.com/v2/prices/spot?currency=USD"])
+    response = subprocess.check_output(['curl', '-s', api])
 
     #json response to string
     json_response = json.loads(response)
 
-    # Print entire json response
-    print(json_response)
-
-    # Pull out balance field 
-    balance=json_response['data']['amount']
-    print(str(balance))
-    return(str(balance))
+    # Parse json response and get timestamps
+    #print(json_response)
+    ts = time.time()
+    base = json_response['data']['base']
+    currency = json_response['data']['currency']
+    amount = json_response['data']['amount']
+    print('[%s] [GET_PRICE] %s/%s %s' % (datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'), base, currency, amount))
+    return(str(amount))
 
 if __name__ == '__main__':
     epd = epd2in13_V2.EPD()     # get the display
     epd.init(epd.FULL_UPDATE)   # initialize the display
-    print("Clear...")           # prints to console, not the display, for debugging 
+    print('[STARTUP]   Clearing the display')           # prints to console, not the display, for debugging 
     epd.Clear(0xFF)             # clear the display and set to white
-   
+    initial_display = True
+
     while True:
         cur_btcPrice = getPrice()
-        printToDisplay(cur_btcPrice)
-        time.sleep(20)
+        printToDisplay(cur_btcPrice, initial_display)
+        time.sleep(60)
